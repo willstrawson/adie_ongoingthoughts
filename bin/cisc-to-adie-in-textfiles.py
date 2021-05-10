8acb46e176e1a2fbdfc84d9885bbccaff4d61236
William Strawson <ws231@sn-analysis03.hpc.susx.ac.uk>
2021-05-10 17:02:15 +0100

    id match now works

--- bin/cisc-to-adie-in-textfiles.py
+++ bin/cisc-to-adie-in-textfiles.py
@@ -5,11 +5,11 @@ from adie import convert
 import os
 import glob
 
-dataloc = '/research/cisc2/projects/critchely_adie/BIDS_data/'
+dataloc = '/research/cisc2/projects/critchley_adie/BIDS_data/'
 
 # access conversion dict 
 conversion_file = 'sourcedata/adie_idconvert.txt'
-rename = adie.convert.convert_dict(os.path.join(dataloc, conversion_file))
+rename = convert.convert_dict(os.path.join(dataloc, conversion_file))
 
 # get all file paths to .tsvs 
 files_to_conv = glob.glob(os.path.join(dataloc, 'sub-*/ses-*/sub-*_scans.tsv'))
@@ -22,8 +22,8 @@ for fl in files_to_conv:
 
 
 # identify and save lines containing names 
-idmatch(string-from-txt-file, rename) # this function should return both cisc and adie id if found in string 
-
+oneid, twoid = convert.idmatch(str(l), rename) # this function should return both cisc and adie id if found in string 
+print (oneid,twoid)
 # replace cisc names wth ADIE names 
 
 # save files 

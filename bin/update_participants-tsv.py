e773b28b7a2e4c88743a47b8d6691bc9a5886350
William Strawson <ws231@sn-analysis03.hpc.susx.ac.uk>
2021-05-13 15:19:03 +0100

    script deployable

--- bin/update_participants-tsv.py
+++ bin/update_participants-tsv.py
@@ -7,8 +7,19 @@ import pandas as pd
 data = ['/research/cisc2/projects/critchley_adie/BIDS_data/derivatives/phenotype/controls/demographics.tsv', \
 '/research/cisc2/projects/critchley_adie/BIDS_data/derivatives/phenotype/patients/demographics.tsv']
 # concatanate these data frames into one
-df = pd.concat([pd.read_csv(data[0]), pd.read_csv(data[1])])
+df = pd.concat([pd.read_csv(data[0], sep='\t'), pd.read_csv(data[1], sep='\t')])
 
 # read in target dataframe
-target = pd.read_csv('/research/cisc2/projects/critchley_adie/BIDS_data/participants.tsv')
+target_path = ('/research/cisc2/projects/critchley_adie/BIDS_data/participants.tsv')
+target = pd.read_csv(target_path)
 
+# print dataframes
+print(df)
+print(target)
+
+# merge gender columns from dfs into target on participant key 
+merged=pd.merge(left=df[['participant_id','Age','GenderID','GenderBirth']], right=target, on=['participant_id'],how='outer')
+print(merged)
+
+#save 
+merged.to_csv(target_path,sep='\t')
